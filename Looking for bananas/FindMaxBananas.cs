5fc882f4acd271d4efe110c59cc1b6ff4ec1bf0e
Janderson <janderson.candido@gmail.com>
2021-09-27 11:08:38 -0300

    initial commit

---
+++ Looking for bananas/FindMaxBananas.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Looking_for_bananas
+{
+    public class FindMaxBananas
+    {
+        public int[,] array { get; set; }
+
+        public int maxBananas { get; set; }
+
+        public List<string> paths { get; set; }
+
+        public FindMaxBananas(int[,] array)
+        {
+            int lengthy = array.GetLength(0);
+            int lengthx = array.GetLength(1);
+
+            this.array = array;
+            this.maxBananas = 0;
+            this.paths = new List<string>();
+        }
+
+        public void findMaxBananas()
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                percorrerPossiveisCaminhos(i, 0, array, 0, "");
+            }            
+        }
+
+        private void percorrerPossiveisCaminhos(int positionI, int positionX, int[,] array, int sum, string path)
+        {
+            if(path == "")
+            {
+                path += $"({positionI},{positionX})";
+            }
+            else
+            {
+                path += $"->({positionI},{positionX})";
+            }
+            
+            sum += array[positionI, positionX];
+
+            int lengthy = array.GetLength(0);
+            int lengthx = array.GetLength(1);
+
+            if (positionX == lengthx - 1)
+            {
+                finalizarSomaFimDoMovimento(sum, path);
+                return;
+            }
+
+            List<int> possiveisMovimentosY = new List<int>();
+            int nextPositionX = positionX;
+
+            if (positionI - 1 >= 0)
+            {
+                possiveisMovimentosY.Add(positionI - 1);
+            }
+
+            possiveisMovimentosY.Add(positionI);
+
+            if (positionI + 1 <= (lengthy - 1))
+            {
+                possiveisMovimentosY.Add(positionI + 1);
+            }
+
+            if (positionX + 1 <= lengthx)
+            {
+                nextPositionX++;
+            }
+
+            if (nextPositionX <= lengthx - 1)
+            {
+                for (int i = 0; i < possiveisMovimentosY.Count; i++)
+                {
+                    percorrerPossiveisCaminhos(possiveisMovimentosY[i], nextPositionX, array, sum, path);
+                }
+            }
+        }
+
+        private void finalizarSomaFimDoMovimento(int soma, string path)
+        {
+            if (soma > maxBananas)
+            {
+                maxBananas = soma;
+                paths = new List<string>();
+                paths.Add(path);
+            }
+            else if (soma == maxBananas)
+            {
+                paths.Add(path);
+            }
+        }
+    }
+}
---
+++ Looking for bananas/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Looking_for_bananas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[4, 4] {
+               {10, 33, 13, 15} ,
+               {22, 21, 4, 1 } ,
+               {5, 0, 2, 3},
+               {0, 6, 14, 2},
+            };
+
+            FindMaxBananas obj = new FindMaxBananas(a);
+            obj.findMaxBananas();
+            Print2DArray(a);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine($"Quantidade máxima de {obj.maxBananas}");
+
+            Console.WriteLine("Path percorrrido");
+            foreach (var path in obj.paths)
+            {
+                Console.WriteLine(path);
+            }
+
+            a = new int[3, 3] {
+               {1, 3, 3} ,
+               {2, 1, 4} ,
+               {0, 6, 4}
+            };
+
+            obj = new FindMaxBananas(a);
+            obj.findMaxBananas();
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            Print2DArray(a);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine($"Quantidade máxima de {obj.maxBananas}");
+
+            Console.WriteLine($"Path percorrrido");
+            foreach (var path in obj.paths)
+            {
+                Console.WriteLine(path);
+            }
+
+            a = new int[4, 4] {
+               {1, 3, 1, 5} ,
+               {2, 2, 4, 1} ,
+               {5, 0, 2, 3},
+               {0, 6, 1, 2},
+            };
+
+            obj = new FindMaxBananas(a);
+            obj.findMaxBananas();
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            Print2DArray(a);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine($"Quantidade máxima de {obj.maxBananas}");
+
+            Console.WriteLine($"Path percorrrido");
+            foreach (var path in obj.paths)
+            {
+                Console.WriteLine(path);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void Print2DArray<T>(T[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
